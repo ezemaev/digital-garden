ddd89f35cd549a4de860cfc2a8d72b6bdb33e74e
ezemaev <191023054+ezemaev@users.noreply.github.com>
2025-05-28 10:13:03 +0530

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "digital-garden-7ht.pages.dev",
+    baseUrl: "ezemaev.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
